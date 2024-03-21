7dfee6d8533a1bba652405f186f782e64b43a41c
aharon <ahla85@gmail.com>
2024-03-21 11:14:30 +0200

    first presale test

---
+++ test/Presale.t.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.23;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {Presale} from "../src/Presale.sol";
+
+contract CounterTest is Test {
+
+    Presale public presale;
+    function setUp() public {
+        presale = new Presale(address(this));
+    }
+
+    function testSetRound() public {
+        presale.setRound(1, block.timestamp, 3600, 10**6, 100);
+        (
+            uint256 startTime, 
+            uint256 duration, 
+            uint256 size, 
+            uint256 price, 
+            uint256 sold
+        ) = presale.rounds(1);
+        assertEq(startTime, block.timestamp);
+        assertEq(duration, 3600);
+        assertEq(size, 10**6);
+        assertEq(price, 100);
+        assertEq(sold, 0);
+        assertEq(presale.currentRound(), 0);
+    }
+}
