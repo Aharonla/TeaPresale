d16ed4393cb6b67ff2613186e1ba82d8461adde3
aharon <ahla85@gmail.com>
2024-03-21 12:06:58 +0200

    test forceStartRound

--- test/Presale.t.sol
+++ test/Presale.t.sol
@@ -42,4 +42,26 @@ contract CounterTest is Test {
         presale.setRound(1, block.timestamp, 3600, 10**6, 100);
     }
 
+    function test_ForceSetRound() public {
+        presale.setRound(1, block.timestamp, 3600, 10**6, 100);
+        vm.expectRevert(abi.encodeWithSelector(Presale.RoundAlreadyExists.selector, 1));
+        presale.setRound(1, block.timestamp, 3600, 10**6, 100);
+        vm.expectEmit(true, true, true, true);
+        emit Presale.SetRound(1, block.timestamp, 3600, 10**6, 100);
+        presale.forceSetRound(1, block.timestamp, 3600, 10**6, 100);
+        (
+            uint256 startTime, 
+            uint256 duration, 
+            uint256 size, 
+            uint256 price, 
+            uint256 sold
+        ) = presale.rounds(1);
+        assertEq(startTime, block.timestamp);
+        assertEq(duration, 3600);
+        assertEq(size, 10**6);
+        assertEq(price, 100);
+        assertEq(sold, 0);
+        assertEq(presale.currentRound(), 0);
+    }
+
 }
