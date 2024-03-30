c53612863d101f50af4bb9c11e4967f018a4230c
aharon <ahla85@gmail.com>
2024-03-30 19:42:04 +0300

    add natSpecs

--- src/Presale.sol
+++ src/Presale.sol
@@ -32,6 +32,11 @@ contract Presale is ERC20, Ownable, Pausable {
         _;}
 
     /// @notice Round parameters
+    /// @param startTime Start time of the round
+    /// @param duration Duration of the round
+    /// @param size Amount of tokens sold in the round
+    /// @param price Price of a token in the round
+    /// @param sold Amount of tokens sold in the round
     struct Round {
         uint256 startTime;
         uint256 duration;
@@ -40,6 +45,9 @@ contract Presale is ERC20, Ownable, Pausable {
         uint256 sold;
     }
 
+    /// @notice Referral parameters
+    /// @param amountSold Amount of tokens sold by the referral
+    /// @param numOfReferrals Number of referrals made by the referral
     struct Referral {
         uint256 amountSold;
         uint256 numOfReferrals;
@@ -51,10 +59,14 @@ contract Presale is ERC20, Ownable, Pausable {
     /// @notice Mapping of round number to round parameters
     mapping(uint8 roundId => Round round) public rounds;
 
+    /// @notice Mapping of referral code to referral parameters
     mapping(uint8 referralId  => Referral referral) public referrals;
 
+    /// @notice Mapping of payment tokens to their status
     mapping(ERC20 token => bool allowed) public paymentTokens;
 
+    /// @notice Constructor
+    /// @param _paymentTokens List of payment tokens
     constructor(address[] memory _paymentTokens) Ownable(_msgSender()) ERC20("TEAPresale", "TPS") {
         for(uint256 i = 0; i < _paymentTokens.length; i++) {
             paymentTokens[ERC20(_paymentTokens[i])] = true;
@@ -62,39 +74,57 @@ contract Presale is ERC20, Ownable, Pausable {
         }
     }
 
+    /// @notice Returns the end time of the current round
     function getRoundEnd() public view returns (uint256) {
         return rounds[currentRound].startTime + rounds[currentRound].duration;
     }
 
+    /// @notice Returns the price of a token in the current round
     function getPrice() public view returns (uint256) {
         return rounds[currentRound].price;
     }
 
+    /// @notice Returns the size of the current round
     function getRoundSize() public view returns (uint256) {
         return rounds[currentRound].size;
     }
 
+    /// @notice Returns the amount of tokens sold in the current round
     function getRoundSold() public view returns (uint256) {
         return rounds[currentRound].sold;
     }
 
+    /// @notice Pauses the contract
     function pause() public onlyOwner {
         _pause();
     }
 
+    /// @notice Unpauses the contract
     function unpause() public onlyOwner {
         _unpause();
     }
 
+    /// @notice Adds a token to the list of payment tokens
+    /// @param _token Address of the token to add
     function addPaymentToken(address _token) public onlyOwner {
         paymentTokens[ERC20(_token)] = true;
         emit AddPaymentToken(_token);
     }
 
+    /// @notice Removes a token from the list of payment tokens
+    /// @param _token Address of the token to remove
     function removePaymentToken(address _token) public onlyOwner {
         paymentTokens[ERC20(_token)] = false;
         emit RemovePaymentToken(_token);
     }
+
+    /// @notice Sets round parameters for the presale
+    /// @dev Used internally by setRound and forceSetRound
+    /// @param _round Round index
+    /// @param _startTime Start time of the round
+    /// @param _duration Duration of the round
+    /// @param _size Amount of tokens sold in the round
+    /// @param _price Price of a token in the round
     function _setRound(
         uint8 _round, 
         uint256 _startTime, 
@@ -107,6 +137,11 @@ contract Presale is ERC20, Ownable, Pausable {
     }
 
     /// @notice Sets round parameters for the presale
+    /// @param _round Round index
+    /// @param _startTime Start time of the round
+    /// @param _duration Duration of the round
+    /// @param _size Amount of tokens sold in the round
+    /// @param _price Price of a token in the round
     function setRound(
         uint8 _round,
         uint256 _startTime, 
@@ -120,6 +155,13 @@ contract Presale is ERC20, Ownable, Pausable {
         _setRound(_round, _startTime, _duration, _size, _price);
     }
 
+    /// @notice Sets round parameters for the presale
+    /// @dev Used in case the round has already started
+    /// @param _round Round index
+    /// @param _startTime Start time of the round
+    /// @param _duration Duration of the round
+    /// @param _size Amount of tokens sold in the round
+    /// @param _price Price of a token in the round
     function forceSetRound(
         uint8 _round,
         uint256 _startTime, 
@@ -133,6 +175,7 @@ contract Presale is ERC20, Ownable, Pausable {
         _setRound(_round, _startTime, _duration, _size, _price);
     }
 
+    /// @notice Starts the next round
     function startNextRound() public onlyOwner {
         if(rounds[currentRound].startTime + rounds[currentRound].duration >= block.timestamp) {
             revert PreviousRoundActive(currentRound);
@@ -142,6 +185,10 @@ contract Presale is ERC20, Ownable, Pausable {
         }
     }
 
+    /// @notice Used to buy tokens in the current round
+    /// @param _amount Amount of tokens to buy
+    /// @param _referral Referral code
+    /// @param _paymentToken Address of the payment token
     function buyTokens(uint256 _amount, uint8 _referral, address _paymentToken) 
     public 
     payable 
@@ -170,9 +217,10 @@ contract Presale is ERC20, Ownable, Pausable {
         emit BuyTokens(_msgSender(), _amount, _referral);
     }
 
+    /// @notice Withdraws the balance of the contract
+    /// @param paymentToken Address of the token to withdraw
     function withdraw(address paymentToken) public onlyOwner {
         emit Withdraw(_msgSender(), ERC20(paymentToken).balanceOf(address(this)));
         ERC20(paymentToken).transfer(_msgSender(), ERC20(paymentToken).balanceOf(address(this)));
     }
-
 }
